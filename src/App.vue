de101fc320cbc759f8d3fc09602705134f16624b
xing-ting-yuan <59352562+xing-ting-yuan@users.noreply.github.com>
2020-02-19 15:32:36 +0800

    项目起步-导入elemnet-ui

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
-<!-- 要加 to 属性 不然就会报错 -->
+<div id="app">
+  <!-- 要加 to 属性 不然就会报错 -->
   <router-link to="#">点我呀</router-link>
+   <el-button  type="primary" disabled>默认按钮</el-button>
+</div>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+//导入UI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+//注册UI
+Vue.use(ElementUI);
 //导入路由文件
 import router from "./router/index.js";
 Vue.config.productionTip = false
