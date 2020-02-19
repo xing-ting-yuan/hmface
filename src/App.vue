542a84157af2042eb70d40ed9ad6fa097c789dee
xing-ting-yuan <59352562+xing-ting-yuan@users.noreply.github.com>
2020-02-19 14:56:10 +0800

    项目起步-路由搭建

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<!-- 要加 to 属性 不然就会报错 -->
+  <router-link to="#">点我呀</router-link>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+//导入路由
+import VueRouter from 'vue-router'
+
+//注册路由
+Vue.use(VueRouter)
+
+//准备组件
+
+//设置路由规则
+const routes  = []
+
+//创建路由对象
+const router = new VueRouter ({
+    routes
+})
+export default router
